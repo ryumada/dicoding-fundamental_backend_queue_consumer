179acd6479374bddaa4fc9d50e748d6bccfc73d1
ryumada <rizukiryumada@gmail.com>
2021-12-15 09:01:58 +0700

    creted main rabbitmq consumer server

---
+++ src/consumer.js
@@ -0,0 +1,24 @@
+// baris ini digunakan untuk mengonsumsi konfigurasi dari file .env
+require('dotenv').config();
+const amqp = require('amqplib');
+const NotesService = require('./NotesService');
+const MailSender = require('./MailSender');
+const Listener = require('./listener');
+const queueName = 'export:notes';
+
+const init = async () => {
+  const notesService = new NotesService();
+  const mailSender = new MailSender();
+  const listener = new Listener(notesService, mailSender);
+
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+  const channel = await connection.createChannel();
+
+  await channel.assertQueue(queueName, {
+    durable: true,
+  });
+
+  channel.consume(queueName, listener.listen, { noAck: true });
+}
+
+init();
